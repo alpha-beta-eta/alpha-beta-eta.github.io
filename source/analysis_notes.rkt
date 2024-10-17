5be39cfb10ba508b31b3565fa37d5f50db039361
alpha-beta-eta <95096031+alpha-beta-eta@users.noreply.github.com>
2024-10-17 00:10:02 -0400

    foo

--- source/analysis_notes.rkt
+++ source/analysis_notes.rkt
@@ -6,6 +6,7 @@
    #:title "分析笔记"
    #:css "styles.css"
    (H1 "分析笔记")
+   (P "希望我有朝一日能学会分析, 结束自大学一年级以来的梦魇.")
    (columnize
     (H2 (A "分析一 (Amann &amp; Escher)" #:attr* '((href "analysis1_amann.html"))))
     (H2 (A "分析三 (Amann &amp; Escher)" #:attr* '((href "analysis3_amann.html"))))
@@ -15,6 +16,7 @@
     (H2 (A "无穷小演算" #:attr* '((href "calculus_dieudonne.html"))))
     (H2 (A "无穷级数的理论和应用" #:attr* '((href "series.html"))))
     (H2 (A "流形上的分析" #:attr* '((href "analysis_munkres.html"))))
+    (H2 (A "分析学练习" #:attr* '((href "exercises_in_analysis.html"))))
     
     )
    ))
---
+++ source/exercises_in_analysis.rkt
@@ -0,0 +1,20 @@
+#lang racket
+(provide exercises_in_analysis.html)
+(require SMathML)
+(define exercises_in_analysis.html
+  (TmPrelude
+   #:title "分析学练习"
+   #:css "styles.css"
+   (H1 "分析学练习")
+   (P "这本书看上去内容符合我的期望, 我想我或许可以通过这本书学会分析.")
+   (H2 "第1章 度量空间")
+   
+   (H2 "第2章 拓扑空间")
+   
+   (H2 "第3章 测度, 积分和鞅")
+   
+   (H2 "第4章 测度和拓扑")
+   
+   (H2 "第5章 泛函分析")
+   
+   ))
