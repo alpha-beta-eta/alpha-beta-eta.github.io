a4bfcc4e1f7d3c69c38cb15050a8b10259427d5d
alpha-beta-eta <95096031+alpha-beta-eta@users.noreply.github.com>
2025-02-27 23:31:55 -0500

    0

---
+++ source/refal.rkt
@@ -0,0 +1,19 @@
+#lang racket
+(provide refal.html)
+(require SMathML)
+(define refal.html
+  (TnTmPrelude
+   #:title "REFAL-5编程指南和参考手册"
+   #:css "styles.css"
+   (H1. "REFAL-5编程指南和参考手册")
+   (H2. "基本的REFAL")
+   (H3. "一个简单的函数定义")
+   (H3. "符号和表达式")
+   (H3. "模式匹配")
+   (H3. "句子和程序")
+   (H2. "计算机中的REFAL")
+   (H2. "基本的编程技巧")
+   (H2. "扩展的REFAL")
+   (H2. "程序建立")
+   (H2. "元系统转换")
+   ))
